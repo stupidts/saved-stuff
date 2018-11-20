efdc9fd6e1c7ca3292a4cdfe9e3dc605b49f315b
WHOOOOOOOOOOOO <37454664+WHOOOOOOOOOOOO@users.noreply.github.com>
2018-11-20 08:32:57 -0500

    8:32/11/20/2018

--- Traveller game/Traveller game/Classes.h
+++ Traveller game/Traveller game/Classes.h
@@ -1,11 +1,15 @@
 #ifndef _CLASSES_H_
 #define _CLASSES_H_
 
-struct Sprite
-{// a lot more stuff for milestone 3
-public:
-	char m_cOutput;
+#include <iostream>
 
+#define ROWS 24
+#define COLS 32
+
+
+struct Sprite
+{
+	char m_cOutput;
 };
 
 class BGTile : public Sprite
@@ -13,24 +17,99 @@ class BGTile : public Sprite
 public:
 	bool m_bIsObstacle;
 	bool m_bIsHazard;
+
 	BGTile()
 	{
 		m_bIsObstacle = false;
 		m_bIsHazard = false;
 	}
+
+	void SetBGTile(const char c)
+	{
+		m_cOutput = c;
+		if (m_cOutput == '*') // * turns into
+			m_cOutput = ' ';  // blank space
+		if (m_cOutput == '+') // + in the file so it's easier to notice in a char mess
+			m_cOutput = 'D';  // Door character
+							  // vvv checks if the char is a Stone, Moss Stone, wood wall, roof tile, tree stump, full tree or a dungeon brick vvv
+		if (m_cOutput == 'M' || m_cOutput == 'm' || m_cOutput == '=' || m_cOutput == 'H' || m_cOutput == 'O' || m_cOutput == '#' || m_cOutput == 'B') 
+			m_bIsObstacle = true;
+		if (m_cOutput == 'X') // is it a hazard?
+			m_bIsHazard = true;
+	}
 };
 
-/* Important note: x coordinate is player's COLUMN position and
-   y coordinate is player's ROW position. Don't mix these up! */
-
-class Player
+class LOTile : public Sprite
 {
 public:
 	int m_x;
 	int m_y;
-	Player(int x, int y) : m_x(x), m_y(y) // Note the initializers for the properties and empty {}.
+	LOTile() {}
+	LOTile(int x, int y) : m_x(x), m_y(y) {}
+};
+
+class Player : public LOTile
+{
+public:
+	Player(int x, int y) : LOTile(x, y)
 	{
-		// Nothing needed in here.
+		m_cOutput = '@'; // player char
+	}
+};
+
+class Door : public LOTile
+{
+private:
+	int m_iToLevel;
+	int m_iDestX;
+	int m_iDestY;
+public:
+	Door()
+	{
+		m_cOutput = 'D'; // D is for door
+	}
+
+	void GetDoor(int tl, int dx, int dy) // accesses the private variables from the outside cold harsh world
+	{
+		tl = m_iToLevel;
+		dx = m_iDestX;
+		dy = m_iDestY;
+		std::cout << "tl:" << std::to_string(tl) << ", dx:" << std::to_string(dx) << ", dy:" << std::to_string(dy) << std::endl;
+	}
+
+	void SetDoor(const int x, const int y, const int tl, const int dx, const int dy) // sets a doors location, file and destination
+	{
+		m_x = x;
+		m_y = y;
+		m_iToLevel = tl;
+		m_iDestX = dx;
+		m_iDestY = dy;
+	}
+};
+
+class Level : public Door
+{
+public:
+	int m_iNumDoors;
+	Door doors[3];
+
+	BGTile map[ROWS][COLS];
+
+	Level()
+	{
+		m_iNumDoors = 0;
+	}
+
+	void AddDoor(const int x, const int y, const int tl, const int dx, const int dy) // makes a door...
+	{
+		std::cout << "H AddDoor()" << std::endl;
+		if (m_iNumDoors < 3)
+		{
+			std::cout << m_iNumDoors << std::endl;
+			doors[m_iNumDoors].SetDoor(x, y, tl, dx, dy); // gets the coordinates from whatever universe it came from
+			std::cout << "H AddDoor(), SetDoor()" << std::endl;
+			m_iNumDoors++;
+		}
 	}
 };
 #endif
--- Traveller game/Traveller game/Main.cpp
+++ Traveller game/Traveller game/Main.cpp
@@ -1,100 +1,115 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <conio.h> // Sorry non-Windows users. 
 #include "Classes.h"
 using namespace std;
 #define ROWS 24
 #define COLS 32
 
+/* Important note: x coordinate is player's COLUMN position and
+   y coordinate is player's ROW position. Don't mix these up! */
 
-
-bool BuildMap(BGTile m[][COLS]) // Passing 2D array to function.
+bool BuildMap(Level& l, const char* n) // Passing 2D array to function.
 {
-	ifstream inFile("Level1.txt");
-	char temp;
+	cout << "BuildMap(); " << n << endl;
+	ifstream inFile(n);
 	if (inFile.is_open())
 	{
+		char temp;
 		for (int row = 0; row < ROWS; row++)
 		{
 			for (int col = 0; col < COLS; col++)
 			{
 				inFile >> temp;
-				m[row][col].m_cOutput = temp;
-				if (temp == 'M' || temp == 'm' || temp == '#')
-				{
-					m[row][col].m_bIsObstacle = true;
-				}
-				else if (temp == 'X')
-				{
-					m[row][col].m_bIsHazard = true;
-				}
-
-				if (m[row][col].m_cOutput == '*')
-					m[row][col].m_cOutput = ' ';
+				l.map[row][col].SetBGTile(temp);
 			}
 		}
+		inFile.close();
 		return 0;
 	}
 	return 1;
 }
 
-void PrintMap(BGTile m[][COLS], Player& p)
+void PrintMap(Level& l, const Player& p)
 {
+	bool isDoor = false;
+
 	for (int row = 0; row < ROWS; row++)
 	{
 		for (int col = 0; col < COLS; col++)
 		{
-			if (row == p.m_y && col == p.m_x)
-				cout << "@";
+			for (int i = 0; i < l.m_iNumDoors; i++)
+			{
+				if (row == l.doors[i].m_y && col == l.doors[i].m_x)
+				{
+					isDoor = true;
+				}
+			}
+			if (isDoor)
+				cout << 'D';
+			else if (row == p.m_y && col == p.m_x)
+				cout << p.m_cOutput;
 			else
-				cout << m[row][col].m_cOutput;
+				cout << l.map[row][col].m_cOutput;
 		}
 		cout << endl;
+	}
+}
+
+void CheckDoors(int cl, Player& p, Level& l)
+{
+	cout << "\n\n\nm_iNumDoors " + to_string(l.m_iNumDoors) << endl;
+	for (int i = 0; i < l.m_iNumDoors; i++)
+	{
+		if (l.doors != 0)
+		{
+			l.doors[i].GetDoor(cl, p.m_x, p.m_y);
+			cout << "cl:" << to_string(cl) << ", p.m_x:" << to_string(p.m_x) << ", p.m_y:" << to_string(p.m_y) << endl;
+		}
+		else
+			cout << "\n\n\nSomething went wrong" << endl;
 	}
 }
 
 int main()
 {
 	bool quit = false;
-	BGTile map[ROWS][COLS]; // 2D array of characters.
+	Level levels[5];
 	char input;
 	Player player(COLS / 2, ROWS / 2);
+	int currLevel(0);
 
-	if (BuildMap(map) == 1)
+	if (BuildMap(levels[0], "Level1.txt") == 1)
+	{
 		return 1;
+	}
+
 	while (!quit)
 	{
-		PrintMap(map, player);
+		PrintMap(levels[currLevel], player);
 		input = _getch();
 		switch (input)
 		{
 		case 'w':  // Up.
 		case 'W':
-			if (player.m_y > 0 && !map[player.m_y - 1][player.m_x].m_bIsObstacle) // How can we add to these checks to account for the '#' obstacles?
+			if (player.m_y > 0 && !levels[currLevel].map[player.m_y - 1][player.m_x].m_bIsObstacle)
 				player.m_y--;
-			else if (map[player.m_y - 1][player.m_x].m_bIsHazard)
-				quit = true;
 			break;
 		case 's': // Down.
 		case 'S':
-			if (player.m_y < ROWS - 1 && !map[player.m_y + 1][player.m_x].m_bIsObstacle)
+			if (player.m_y < ROWS - 1 && !levels[currLevel].map[player.m_y + 1][player.m_x].m_bIsObstacle)
 				player.m_y++;
-			else if (map[player.m_y - 1][player.m_x].m_bIsHazard)
-				quit = true;
 			break;
 		case 'a': // Left.
 		case 'A':
-			if (player.m_x > 0 && !map[player.m_y][player.m_x - 1].m_bIsObstacle)
+			if (player.m_x > 0 && !levels[currLevel].map[player.m_y][player.m_x - 1].m_bIsObstacle)
 				player.m_x--;
-			else if (map[player.m_y - 1][player.m_x].m_bIsHazard)
-				quit = true;
 			break;
 		case 'd': // Right.
 		case 'D':
-			if (player.m_x < COLS - 1 && !map[player.m_y][player.m_x + 1].m_bIsObstacle)
+			if (player.m_x < COLS - 1 && !levels[currLevel].map[player.m_y][player.m_x + 1].m_bIsObstacle)
 				player.m_x++;
-			else if (map[player.m_y - 1][player.m_x].m_bIsHazard)
-				quit = true;
 			break;
 		case 'q':
 		case 'Q':
@@ -102,6 +117,44 @@ int main()
 			break;
 		}
 		system("cls");
+		if (levels[currLevel].map[player.m_y][player.m_x].m_bIsHazard)
+		{
+			quit = true;
+			system("cls");
+			cout << "\n\n\n\n\n\n\n\nYou died!\n\n\n\n\n\n\n\n" << endl;
+		}
+		if (levels[currLevel].map[player.m_y][player.m_x].m_cOutput == 'D')
+		{
+			cout << "\n\n\non a door!" << endl;
+			CheckDoors(currLevel, player, *levels);
+			cout << "\n\n\ncurr: " << currLevel << ", px: " << player.m_x << ", py:" << player.m_y << endl;
+			if (currLevel == 0)
+			{
+				cout << "cl 0" << endl;
+				levels[0].AddDoor(14, 20, 2, 17, 14);
+				BuildMap(*levels, "Level1.txt");
+			}
+			if (currLevel == 1)
+			{
+				cout << "cl 1" << endl;
+				levels[1].AddDoor(17, 14, 1, 14, 20);
+				levels[1].AddDoor(27, 15, 3, 17, 14);
+				levels[1].AddDoor(10, 6, 4, 27, 11);
+				BuildMap(*levels, "Level2.txt");
+			}
+			if (currLevel == 2)
+			{
+				cout << "cl 2" << endl;
+				levels[2].AddDoor(17, 14, 2, 27, 15);
+				BuildMap(*levels, "Level3.txt");
+			}
+			if (currLevel == 3)
+			{
+				cout << "cl 3" << endl;
+				levels[3].AddDoor(27, 11, 2, 10, 6);
+				BuildMap(*levels, "Level4.txt");
+			}
+		}
 	}
 	system("pause");
 	return 0;
